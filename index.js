29d5d3c799b156cf3430af8de49efb6f4990187d
yuvaraj1126 <yuvarajgowtham30@gmail.com>
2025-03-26 23:15:09 +0530

    Added cros

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express =require('express')
 const app = express()
-const PORT = ;process.env.PORT || 8000
+const cors =require('cors')
+const PORT = process.env.PORT || 8000
 const USERS =[
     {
     id:1,
@@ -22,11 +23,11 @@ const USERS =[
     name:"iyer",
     email:"iyer@gmail.com"
 }]
-
+//cors - cross origin resource sharing 
 //middleware- it is a function that will be used req,res cycle
 
 app.use(express.json())
-
+app.use(cors())
 app.use((req,res,next)=>{
     console.log(`${req.method}:${req.url}`)
     next()
